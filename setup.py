20fab6599780cacce9c1ceffc83c9725e19fec45
Graham Ullrich <graham@flyingcracker.com>
2018-03-17 14:13:07 -0600

    Remove use of django-user-accounts templatetag
    Update major version for backward incompatible change

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = "1.0.4"
+VERSION = "2.0.0"
 LONG_DESCRIPTION = """
 .. image:: http://pinaxproject.com/pinax-design/patches/pinax-templates.svg
     :target: https://pypi.python.org/pypi/pinax-templates/
